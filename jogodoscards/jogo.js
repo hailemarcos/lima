db1b2519938c3055156c1f3409a4c2e6e1a5d765
hailemarcos <71887920+hailemarcos@users.noreply.github.com>
2021-01-13 10:31:23 -0300

    Add files via upload

---
+++ jogodoscards/jogo.js
@@ -0,0 +1,121 @@
+function verificar_resposta1(){
+    let resposta1 = document.getElementById("1terceira_alternativa").checked
+    if (resposta1 == true){
+        document.getElementById("saida_pergunta1").textContent = "Sigurd"
+        document.getElementById("saida_pontuaçao").textContent = "100"
+    }else{
+        document.getElementById("saida_pergunta1").textContent = "Voce Errou, A Resposta Certa é Sigurd"
+    }
+}
+function verificar_resposta2(){
+let resposta2 = document.getElementById("2primeira_alternativa").checked
+if (resposta2 == true){
+    document.getElementById("saida_pergunta2").textContent = "Ragnar"
+    document.getElementById("saida_pontuaçao").textContent = "200"
+}else{
+    document.getElementById("saida_pergunta2").textContent = "Voce Errou, A Resposta Certa é Ragnar"
+}
+}
+function verificar_resposta3(){
+    let resposta3 = document.getElementById("3segunda_alternativa").checked
+    if (resposta3 == true){
+        document.getElementById("saida_pergunta3").textContent = "Rei Aelle"
+        document.getElementById("saida_pontuaçao").textContent = "300"
+    }else{
+        document.getElementById("saida_pergunta3").textContent = "Voce Errou, A Resposta Certa é Rei Aelle"
+    }
+}
+function verificar_resposta4(){
+    let resposta4 = document.getElementById("4primeira_alternativa").checked
+    if (resposta4 == true){
+        document.getElementById("saida_pergunta4").textContent = "O Vidente"
+        document.getElementById("saida_pontuaçao").textContent = "400"
+    }else{
+        document.getElementById("saida_pergunta4").textContent = "Voce Errou, A Resposta Certa é O Vidente"
+    }
+}
+function verificar_resposta5(){
+    let resposta5 = document.getElementById("5terceira_alternativa").checked
+    if (resposta5 == true){
+        document.getElementById("saida_pergunta5").textContent = "6 Filhos"
+        document.getElementById("saida_pontuaçao").textContent = "500"
+    }else{
+        document.getElementById("saida_pergunta5").textContent = "Voce Errou, A Resposta Certa é 6 Filhos"
+    }
+}
+function verificar_resposta6(){
+    let resposta6 = document.getElementById("6primeira_alternativa").checked
+    if (resposta6 == true){
+        document.getElementById("saida_pergunta6").textContent = "Lagherta A Dama do Escudo"
+        document.getElementById("saida_pontuaçao").textContent = "600"
+    }else{
+        document.getElementById("saida_pergunta6").textContent = "Voce Errou, A Resposta Certa é Lagherta A Dama do Escudo"
+    }
+} 
+function verificar_resposta7(){
+let resposta7 = document.getElementById("7primeira_alternativa").checked
+if (resposta7 == true){
+    document.getElementById("saida_pergunta7").textContent = "Ubbe"
+    document.getElementById("saida_pontuaçao").textContent = "700"
+    }else{
+    document.getElementById("saida_pergunta7").textContent = " Voce Errou,A Resposta Certa é Ubbe" 
+    }
+}
+function verificar_resposta8(){
+    let resposta8 = document.getElementById("8segunda_alternativa").checked
+    if (resposta8 == true){
+        document.getElementById("saida_pergunta8").textContent = "Bjorn Ironside"
+        document.getElementById("saida_pontuaçao").textContent = "800"
+        }else{
+        document.getElementById("saida_pergunta8").textContent = " Voce Errou,A Resposta Certa é Bjorn Ironside" 
+        }
+}
+function verificar_resposta9(){
+    let resposta9 = document.getElementById("9primeira_alternativa").checked
+    if (resposta9 == true){
+        document.getElementById("saida_pergunta9").textContent = "Floki O Louco"
+        document.getElementById("saida_pontuaçao").textContent = "900"
+        }else{
+        document.getElementById("saida_pergunta9").textContent = " Voce Errou,A Resposta Certa Floki O Louco" 
+        }
+}
+function verificar_resposta10(){
+    let resposta10 = document.getElementById("10segunda_alternativa").checked
+    if (resposta10 == true){
+        document.getElementById("saida_pergunta10").textContent = "2°)Gyda lothbrok"
+        document.getElementById("saida_pontuaçao").textContent = "1000"
+        }else{
+        document.getElementById("saida_pergunta10").textContent = " Voce Errou,A Resposta Certa é 2°)Gyda lothbrok" 
+        }
+}   
+function verificar_resposta11(){
+    let resposta11 = document.getElementById("11terceira_alternativa").checked
+    if (resposta11 == true){
+        document.getElementById("saida_pergunta11").textContent = "Athelstan"
+        document.getElementById("saida_pontuaçao").textContent = "1100"
+        }else{
+        document.getElementById("saida_pergunta11").textContent = " Voce Errou,A Resposta Certa é Athelstan" 
+        }
+}
+function verificar_resposta12(){
+    let resposta12 = document.getElementById("12primeira_alternativa").checked
+    if (resposta12 == true){
+        document.getElementById("saida_pergunta12").textContent = "Bispo Heahmund"
+        document.getElementById("saida_pontuaçao").textContent = "1200"
+        }else{
+        document.getElementById("saida_pergunta12").textContent = " Voce Errou,A Resposta Certa é Bispo Heamund" 
+        }
+}  
+    
+    
+        
+      
+    
+    
+    
+    
+    
+
+
+
+
